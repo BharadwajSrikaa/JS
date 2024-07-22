c0924a2a751a7bebf0567fce5073caafa2ce18df
Bharadwaj Srikaa <bharadwajsrikaa@gmail.com>
2024-07-22 15:30:29 +0530

    ShortCuts and notes file Updated

--- 01. Basics/app.js
+++ 01. Basics/app.js
@@ -1,1 +1,23 @@
+let fName = "Mihawk";
+let sName = "Hawkeye";
 
+// 1. Concatination
+// let fullName = fName.concat(sName);
+// console.log(fullName);
+
+// 2. Append
+// fName += "Something Else";
+// console.log(fName);
+
+// 3. Length
+// console.log(fName.length);
+
+// 4. Cases
+// console.log(fName.toLowerCase());
+// console.log(fName.toUpperCase());
+
+// 5. Slice
+// console.log(fName.slice(0, 3));
+
+// 6. Split and Join Methods
+console.log(fName.split(""));
